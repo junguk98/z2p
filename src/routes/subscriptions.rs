3a6eb1d71be8de7b425f7eccf52431ddc568e586
junguk98 <dlwjddnrbb71@naver.com>
2024-01-15 20:04:20 +0900

    add pending_confirmation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,10 @@
 use actix_web::{web, HttpResponse};
+use actix_web::web::Data;
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
+use crate::email_client::EmailClient;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,13 +25,17 @@ impl TryFrom<FormData> for NewSubscriber {
 #[allow(clippy::async_yields_async)]
 #[tracing::instrument(
 name = "Adding a new subscriber",
-skip(form, pool),
+skip(form, pool, email_client),
 fields(
 subscriber_email = % form.email,
 subscriber_name = % form.name
 )
 )]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> HttpResponse {
     let new_subscriber = match form.0.try_into() {
         Ok(new_subscriber) => new_subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
@@ -38,7 +44,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
+    };
+
+    if send_confirmation_email(&email_client, new_subscriber)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
     }
+    HttpResponse::Ok().finish()
 }
 
 #[tracing::instrument(
@@ -55,7 +69,7 @@ pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) ->
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now(),
-        "confirmed"
+        "pending_confirmation"
     )
         .execute(pool)
         .await
@@ -64,4 +78,29 @@ pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) ->
             e
         })?;
     Ok(())
+}
+
+async fn send_confirmation_email(
+    email_client: &Data<EmailClient>,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link =
+        "https://there-is-no-such-domain.com/subscriptions/confirm";
+
+    let plain_body = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+
+    let html_body = format!(
+        "Welcome to our newsletter!<br />\
+        Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+
+    email_client
+        .send_email(
+            new_subscriber.email,
+            "Welcome!", &html_body, &plain_body,
+        ).await
 }
