5fa14949c33a76839e9b54894dbf8ac4fe56bd40
junguk98 <dlwjddnrbb71@naver.com>
2024-01-16 09:57:55 +0900

    add subscriptions_confirm skeleton

--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,4 @@
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
@@ -59,6 +59,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool, email_client: EmailClient) ->
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
